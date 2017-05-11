edbc8abfd6d934d10b2bf84c2b2cf20de8923a1d
當間奈津子 <natuko_1983@hotmail.com>
2017-05-12 03:23:09 +0900

    #11[jQuery]ボタンを押したら、CSSのクラスを操作する

--- js/pop_up.js
+++ js/pop_up.js
@@ -3,18 +3,16 @@ $(function(){
 // モーダルウィンドウが開くときの処理	
 $(".modalOpen").click(function(){
 		
-	var navClass = $(this).attr("class"),
-        href = $(this).attr("href");
-			
-        $(href).fadeIn();
-	$(this).addClass("open");
+	var href = $(this).attr("href");
+    $(href).fadeIn();
+	$("body").addClass('lock');
 	return false;
 });
 
 // モーダルウィンドウが閉じるときの処理	
 $(".modalClose").click(function(){
 	$(this).parents(".modal").fadeOut();
-	$(".modalOpen").removeClass("open");
+	$("body").removeClass("lock");
 	return false;
 });  
     
